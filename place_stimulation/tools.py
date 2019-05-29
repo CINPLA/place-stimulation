f10b136103d9f6719efb92e77006168cf5e2a5a8
Alessio Buccino <alejoe9187@gmail.com>
2019-05-29 12:04:27 +0200

    updates

--- place_stimulation/tools.py
+++ place_stimulation/tools.py
@@ -108,7 +108,7 @@ def load_spiketrains(data_path, channel_group=None, load_waveforms=False, t_star
     return sptr
 
 
-def load_tracking(data_path, select_tracking=None, interp=False, fc=5*pq.Hz, t_start=0 * pq.s):
+def load_tracking(data_path, select_tracking=None, interp=False, reverse_y=True, fc=5*pq.Hz, t_start=0 * pq.s):
     '''
 
     Parameters
@@ -118,6 +118,7 @@ def load_tracking(data_path, select_tracking=None, interp=False, fc=5*pq.Hz, t_s
     select_tracking
     interp
     fc
+    t_start
 
     Returns
     -------
@@ -136,16 +137,14 @@ def load_tracking(data_path, select_tracking=None, interp=False, fc=5*pq.Hz, t_s
         t1 = position_group['led_0']['timestamps'].data
         unit = t1.units
         x1, y1, t1 = rm_nans(x1, y1, t1)
-        t1 = t1 - t_start
-        t1 = t1[np.where(t1 > 0)]
+        t1 = t1 * unit
         led0 = True
     if 'led_1' in position_group.keys():
         x2, y2 = position_group['led_1']['data'].data.T
         t2 = position_group['led_1']['timestamps'].data
         unit = t2.units
         x2, y2, t2 = rm_nans(x2, y2, t2)
-        t2 = t2 - t_start
-        t2 = t2[np.where(t2 > 0)]
+        t2 = t2 * unit
         led1 = True
 
     if select_tracking is None and led0 and led1:
@@ -164,7 +163,7 @@ def load_tracking(data_path, select_tracking=None, interp=False, fc=5*pq.Hz, t_s
 
     dt = np.mean(np.diff(t))
     fs = 1. / dt
-    print(fc, fs)
+    print(fs)
 
     # remove zeros
     idx_non_zero_x = np.where(x != 0)
@@ -180,9 +179,17 @@ def load_tracking(data_path, select_tracking=None, interp=False, fc=5*pq.Hz, t_s
     # x = x[mask]
     # y = y[mask]
     # t = t[mask]
+    tf = tf - t_start
+    idxs = np.where(tf > 0)
+    tf = tf[idxs]
+    xf = xf[idxs]
+    yf = yf[idxs]
 
     vel = np.gradient([xf, yf], axis=1)/dt
     speed = np.linalg.norm(vel, axis=0)
+
+    if reverse_y:
+        yf = np.max(yf) - yf
 
     return xf, yf, tf, speed
 
@@ -260,6 +267,7 @@ def velocity_threshold(x, y, t, threshold):
         1d vector of times at x, y positions
     threshold : float
     """
+    unit = t.units
     assert len(x) == len(y) == len(t), 'x, y, t must have same length'
     r = np.sqrt(np.diff(x)**2 + np.diff(y)**2)
     v = np.divide(r, np.diff(t))
@@ -267,6 +275,7 @@ def velocity_threshold(x, y, t, threshold):
     x[speed_lim] = np.nan
     y[speed_lim] = np.nan
     x, y, t = rm_nans(x, y, t)
+    t = t * unit
     return x, y, t
 
 
@@ -293,8 +302,8 @@ def select_best_position(x1, y1, t1, x2, y2, t2, speed_filter=5):
     x1, y1, t1, x2, y2, t2 = _cut_to_same_len(x1, y1, t1, x2, y2, t2)
     measurements1 = len(x1)
     measurements2 = len(x2)
-    x1, y1, t1 = rm_nans(x1, y1, t1)
-    x2, y2, t2 = rm_nans(x2, y2, t2)
+    # x1, y1, t1 = rm_nans(x1, y1, t1)
+    # x2, y2, t2 = rm_nans(x2, y2, t2)
     if speed_filter is not None:
         x1, y1, t1 = velocity_threshold(x1, y1, t1, speed_filter)
         x2, y2, t2 = velocity_threshold(x2, y2, t2, speed_filter)
@@ -336,8 +345,13 @@ def interp_filt_position(x, y, tm, box_xlen=1 , box_ylen=1 ,
     out : angles, resized t
     """
     import scipy.signal as ss
+    pos_unit = x.units
     assert len(x) == len(y) == len(tm), 'x, y, t must have same length'
-    t = np.arange(tm.min(), tm.max() + 1. / pos_fs * tm.units, 1. / pos_fs * tm.units)
+
+    if not isinstance(pos_fs, pq.Quantity):
+        t = np.arange(tm.min(), tm.max() + 1. / pos_fs * tm.units, 1. / pos_fs * tm.units)
+    else:
+        t = np.arange(tm.min(), tm.max() + 1. / pos_fs, 1. / pos_fs)
     x = np.interp(t, tm, x)
     y = np.interp(t, tm, y)
     # rapid head movements will contribute to velocity artifacts,
@@ -367,6 +381,9 @@ def interp_filt_position(x, y, tm, box_xlen=1 , box_ylen=1 ,
     R = np.sqrt(np.diff(x)**2 + np.diff(y)**2)
     V = R / np.diff(t)
     print('Maximum speed {}'.format(V.max()))
+    t = t * tm.units
+    x = x * pos_unit
+    y = y * pos_unit
     return x, y, t
 
 
